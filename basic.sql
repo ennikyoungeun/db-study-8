0f52037441e7cd7ca0091932365fb6ed53b5d67e
ennikyoungeun <ennikyoungeun@gmail.com>
2026-07-06 15:37:29 +0900

    db시작

---
+++ basic.sql
@@ -0,0 +1,29 @@
+--sql  query 작성영역
+--한줄주석
+/*
+여러줄 
+주석
+처리
+*/
+
+DB(DataBase) 데이터가 저장되는 공간(개념)
+DBMS (DataBase Management System) 데이터 베이스 관리 시스템
+
+PC -> Windows Os -> Oracle DBMS <<-접속 도구 /툴(sqlplus,spldeveloper,DBever,Orange,etc)로 사용자가 접속 후 사용
+                               (SQL 쿼리문을 사용하여 DB 데이터를 사용/조작)
+                               
+
+Oracle DB 서버에 접속
+*접속계정
+id: scott
+pw:tiger
+
+SQL (쿼리문)
+Structured Query Language
+
+DB사용 조작 -> 데이터 조작명령 (쿼리문을 작성해서...)
+CRUD
+Create 생성
+Read   조회 (읽기)
+Update 수정
+Delete 삭제
