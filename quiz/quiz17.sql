fe6fdd26fc38b5a78b0a342598b264151c45d153
ennikyoungeun <ennikyoungeun@gmail.com>
2026-07-15 09:03:36 +0900

    quiz17 두 코드 모두 유지하며 병합 완료

--- quiz/quiz17.sql
+++ quiz/quiz17.sql
@@ -1,6 +1,21 @@
 1. 학생 테이블 (student) 과 학과 테이블 (department) 테이블을 사용하여
 학생이름, 1전공학과번호(deptno1) , 1전공 학과 이름을 출력하세요.
 
+
+
+select * from student;
+select * from department;
+
+select
+s.name,
+s.deptno1,
+d.dname
+from student s, department d
+where s.deptno1 = d.deptno
+order by deptno1; 
+
+--------------t-----
+
 select *
 from student;
 select *
@@ -12,13 +27,38 @@ where s.deptno1 = d.deptno;   --INNER JOIN
 
 
 
+
 2. emp2 테이블과 p_grade 테이블을 조회하여
 현재 직급이 있는 사원의 이름과 직급, 현재 연봉,
 해당 직급의 연봉의 하한금액과 상한 금액을
 아래 결과 화면과 같이 출력하세요.
+=======
+--------------------------
+
+2. emp2 테이블과 p_grade 테이블을 조회하여
+현재 직급이 있는 사원의 이름과 직급, 현재 연봉,
+해당 직급의 연봉의 하한금액과
+상한 금액을 아래 결과 화면과 같이 출력하세요.
+
 
 select * from emp2;
 select * from p_grade;
+
+=======
+select 
+e.name,
+e.position,
+to_char(e.pay,'999,999,999'),
+to_char(p.s_pay,'999,999,999') "low pay",
+to_char(p.e_pay,'999,999,999') "high pay"
+from emp2 e, p_grade p
+where e.pay between p.s_pay and p.e_pay
+and e.position is not null;
+
+--------------t----------------
+select * from emp2;
+select * from p_grade;
+
 
 
 select * from emp2
@@ -39,6 +79,7 @@ and e.position = p.position(+);  --OUTER JOIN
 from emp2 e LEFT OUTER JOIN p_grade p
 ON e.position = p.position;  --OUTER JOIN
 
+
 3. Emp2 테이블과 p_grade 테이블을 조회하여
 사원들의 이름과 나이, 현재 직급 , 예상 직급 을 출력하세요.
 예상 직급은 나이로 계산하며 해당 나이가 받아야 하는 직급을 의미합니다.
@@ -53,6 +94,7 @@ p.position "그나이에 맞는 포지션"
 from emp2 e, p_grade p
 where trunc(months_between(to_date('2010-05-30','yyyy-mm-dd'),e.birthday)/12)
 between p.s_age and p.e_age;
+
 
 -------------------------------------------t-----------------
 ------------------심플한국나이
@@ -80,13 +122,22 @@ trunc(months_between(to_date('2010-05-30'),birthday)/12) 월단위차이계산
 from emp2;
 
 
+=======
+
 
 4 . customer 테이블과 gift 테이블을 Join하여 
 고객이 자기 포인트보다 낮은 포인트의 상품 중 한가지를 선택할 수 있다고 할 때 
 Notebook 을 선택할 수 있는 고객명과 포인트, 상품명을 출력하세요.
 
+
 select * from customer;
 select * from gift;
+=======
+select *
+from customer;
+select *
+from gift;
+
 
 select
 c.gname,
@@ -95,6 +146,7 @@ g.gname
 from customer c, gift g
 where c.point >= g.g_start
 and g.gname = 'Notebook';
+
 -------------------------------t--
 select
 c.gname,
@@ -141,6 +193,8 @@ where point >= (select g_start
 select g_start
 from gift
 where gno = 7;
+
+=======
 
 
 5. professor 테이블에서 교수의 번호, 교수이름, 입사일, 자신보다 입사일 빠른 사람 인원수를 출력하세요.
@@ -211,6 +265,8 @@ ROW_NUMBER() : 공동 순위 없이 번호를 순서대로 부여
 ;
 ******************************************
 
+=======
+
 6. emp 테이블에서 사원번호,
 사원이름, 입사일, 자신보다 먼저 입사한 사람 인원수를 출력하세요. 단 자신보다 입사일이 빠른 사람수를 오름차순으로 출력하세요.
 
@@ -231,3 +287,8 @@ ename,
 hiredate,
 rank() over (order by hiredate ) -1 as "count"
 from emp;
+=======
+where e1.hiredate > e2.hiredate(+)
+group by e1.empno,e1.ename,e1.hiredate
+order by "count";
+
